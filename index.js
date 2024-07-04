cb68169397d052bc2e15f6ff1b56b682b924d475
Erubey04 <erubey.david@gmail.com>
2024-07-04 02:25:09 +0000

    Agregados metodos post, get, getcon id, etc..

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express')
+const db = require('./repository/repoFile')
 const app = express()
 const port = 3000
 
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/cars', async(req, res) => {
+  let carsList = await db.findAll()
+  res.status(200).json(carsList)
+})
+
+app.get('/cars/:carId', async (req, res) => {
+  let car = await db.findById(req.params.carId)
+  if (car) {
+    res.status(200).json(car)
+  } else {
+    res.status(404).json({message: "Car Not Found"})
+  }
 })
 
 app.listen(port, () => {
